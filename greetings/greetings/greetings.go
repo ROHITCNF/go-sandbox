f2a306b87101f0f4981b2452abd6c0e758f0ce62
Rohit Srivastava <rohitcnfsecond@gmail.com>
2026-06-29 12:22:53 +0530

    feat : return a random greeting

--- greetings/greetings/greetings.go
+++ greetings/greetings/greetings.go
@@ -1,20 +1,33 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
 )
 
 //Hello returns a greeting for the named person.
 
-func Hello(name string) (string , error){
-   //return error if name is empty
-   if(name == ""){
-	return "" , errors.New("empty name")
-   }
+func Hello(name string) (string, error) {
+	//return error if name is empty
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	//Return a greeting that embeds the name in a message.
 	var message string
-	message = fmt.Sprintf("Hi %v. Welcome" , name)
+	message = fmt.Sprintf(randomFormat(), name)
 
-	return message , nil
-} 
+	return message, nil
+}
+
+// randomFormat can only be accessible in own package
+func randomFormat() string {
+	//slice of message formats
+	formats := []string{
+		"Hi %v . Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// return a random index of slices
+	return formats[rand.Intn(len(formats))]
+}
--- greetings/hello/hello.go
+++ greetings/hello/hello.go
@@ -1,17 +1,19 @@
 package main
+
 import (
 	"fmt"
 	"log"
+
 	"example.com/greetings"
 )
 
-func main (){
+func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
 	//Request a greeting message.
-	message , err := greetings.Hello("tetet")
-	if(err !=nil){
+	message, err := greetings.Hello("Golang")
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
